2e9d181e42029eccd94a59108a7f9f56444bd2af
Henrique Moreira <henriquemoreiraa@gmail.com>
2021-09-17 19:49:25 -0300

    Insert user

---
+++ src/main/java/com/iftm/postsMongoDB/config/TestConfig.java
@@ -0,0 +1,34 @@
+package com.iftm.postsMongoDB.config;
+
+import java.util.Arrays;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.iftm.postsMongoDB.models.entities.User;
+import com.iftm.postsMongoDB.repositories.UserRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	// executa o metodo assim que o contexto da app
+	// for iniciada, ou seja, depois que o componente foi criado.
+	@PostConstruct
+	public void init() {
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Gray", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+}
