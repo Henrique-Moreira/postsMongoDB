872881f3d4e50f4f285970ad85a3008d32b6b6b4
Henrique Moreira <henriquemoreiraa@gmail.com>
2021-09-25 20:19:51 -0300

    POST User

--- src/main/java/com/iftm/postsMongoDB/controllers/UserController.java
+++ src/main/java/com/iftm/postsMongoDB/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.iftm.postsMongoDB.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.iftm.postsMongoDB.models.dto.UserDTO;
 import com.iftm.postsMongoDB.services.UserService;
@@ -29,5 +33,12 @@ public class UserController {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		UserDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody UserDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
